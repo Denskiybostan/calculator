39daa5db8ad3b9e3e302ec7ba805ed5c303c1e2f
denis <deniz246@mail.ru>
2023-12-25 15:14:31 +0300

    проверьте пожалуйста

---
+++ src/main/java/org/example/calculator/service/CalculatorService.java
@@ -0,0 +1,22 @@
+package org.example.calculator.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService{
+    public int plus(int val1, int val2){
+        return val1 + val2;
+    }
+
+    public int minus(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+}
